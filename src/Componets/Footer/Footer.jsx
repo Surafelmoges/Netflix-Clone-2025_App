2558fca090f9a17e70fc766005e20553920d8f49
Surafelmoges <surafelmoges01@gmail.com>
2025-11-27 06:33:53 +0000

    Style the Header and Footer Section

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
-import './footer.css'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import './footer.css';
 
 function Footer() {
   return (
-    <div>
-        <h1>Footer Section</h1>
-    </div>
+         <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        
+        {/* 1. Social Icons */}
+        <div className='footer_icons'>
+            <FacebookOutlinedIcon />
+            <InstagramIcon />
+            <YouTubeIcon />
+        </div>
+
+        {/* 2. The Link Data (4 Columns) */}
+        <div className='footer_data'>
+            <ul>
+                <li>Audio Description</li>
+                <li>Investor Relations</li>
+                <li>Legal Notice</li>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                <li>Cookie Preferences</li>
+                <li>Gift Cards</li>
+                <li>Terms of Use</li>
+                <li>Corporate Information</li>
+                <li>Media Center</li>
+                <li>Privacy</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+
+        {/* 3. Service Code & Copyright */}
+        <div className='service_code'>
+            <p>
+                Service Code
+            </p>
+        </div>
+        <div className='copy-write'>
+            &copy; 1997-2024 Netflix, Inc, Developed by <strong>Surafel Moges</strong>
+        </div>
+
+
+         </div>
+          </div>
   )
 }
 
--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -2,33 +2,36 @@ import React from 'react'
 import './header.css'
 import logo from '../../assets/logo.png'
 import SearchIcon from '@mui/icons-material/Search';
-
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 function Header() {
   return (
+       <div className='outerContainer'>
+    {/* Use specific HTML tags like <nav> or <header> for better accessibility */}
+    <div className="headerContainer"> 
+        
+        {/* 1. LEFT SIDE */}
+        <div className="header-left">
+            <ul>
+                 {/* It is okay to keep logo in li, but often easier to separate it */}
+                <li className='logo'><img src={logo} alt="Netflix-logo" /></li>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li> 
+                <li>New & Popular</li>
+                <li>My List</li>
+                <li>Browse by Languages</li>
+            </ul>
+        </div>
 
-    <div className='outerContainer'>
-        <div className="headerContainor">
-            <div className="header-left">
-                {/* 1. LEFT SIDE - Logo and Navigation Links */}
-                <ul>
-                    <li className='logo'><img src={logo} alt="Netflix-logo" /></li>
-                    <li>Home</li>
-                    <li>TV Shows</li>
-                    <li>Movies</li> 
-                    <li>New & Popular</li>
-                    <li>My List</li>
-                    <li>Browse by Languages</li>
-                </ul>
-            </div>
-            <div className="header-right">
-                {/* 2. RIGHT SIDE - Search, Kids, Notifications, User Profile */}
-                <ul>
-                    <li><SearchIcon /></li>
-                    <li>KIDS</li>
-                    <li>🔔</li>
-                    <li>👤</li>
-                </ul>
-            </div>
+        {/* 2. RIGHT SIDE */}
+        <div className="header-right">
+            <ul>
+                <li><SearchIcon /></li>
+                <li><NotificationsNoneIcon /></li>
+                <li><AccountBoxIcon/></li>
+            </ul>
+        </div>
 
   </div>
 </div>
